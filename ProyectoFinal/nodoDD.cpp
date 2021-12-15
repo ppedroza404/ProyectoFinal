0a44e3bf20a0971a0e4981f01b99953e72760ed4
ajimenezr1 <ajimenezr1@ucenfotec.ac.cr>
2021-12-15 01:29:51 -0600

    Se actualizado el nopdo de duende

--- ProyectoFinal/nodoDD.cpp
+++ ProyectoFinal/nodoDD.cpp
@@ -1,1 +1,40 @@
 #include "nodoDD.h"
+
+nodoDD::nodoDD()
+{
+}
+
+nodoDD::nodoDD(Duende* duende)
+{
+	this->duende = NULL;
+}
+
+Duende* nodoDD::getDuende()
+{
+	return this->duende;
+}
+
+nodoDD* nodoDD::getSgte()
+{
+	return this->sgte;
+}
+
+nodoDD* nodoDD::getAnterior()
+{
+	return this->anterior;
+}
+
+void nodoDD::setDuende(Duende* duende)
+{
+	this->duende = duende;
+}
+
+void nodoDD::setSgte(nodoDD* sgte)
+{
+	this->sgte = sgte;
+}
+
+void nodoDD::setAnterior(nodoDD* anterior)
+{
+	this->anterior = anterior;
+}
